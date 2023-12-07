6ecfbcaf3990809af5ced27893d3c539d0a67a09
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-12-08 01:36:30 +0300

    with sorting

--- src/components/ListBooks.vue
+++ src/components/ListBooks.vue
@@ -3,6 +3,11 @@
       <NavigationMenu />
       <h1>List of Books</h1>
       <div class="divider"></div>
+      <select v-model="selectedSort" @change="sortBooks">
+        <option value="title">Сортировка по алфавиту названий</option>
+        <option value="price_asc">Сортировка по возрастанию цены</option>
+        <option value="price_desc">Сортировка по убыванию цены</option>
+      </select>
       <div class="book-container">
         <div v-for="book in books" :key="book.title" class="book-card">
           <img src="/images/img.png" alt="Изображение книги">
@@ -10,7 +15,6 @@
             <h4>{{ book.title }}</h4>
             <p>{{ book.price }}</p>
             <a :href="book.bookUrl" class="btn btn-primary btn-buy">More</a>
-            <!-- Форма покупки и другие элементы -->
           </div>
         </div>
       </div>
@@ -28,13 +32,31 @@ export default {
   },
   data() {
     return {
-      books: []
+      books: [],
+      selectedSort: 'title' // значение по умолчанию для сортировки
     };
+  },
+  methods: {
+    sortBooks() {
+      if (this.selectedSort === 'title') {
+        this.books.sort((a, b) => a.title.localeCompare(b.title));
+      } else {
+        this.books.sort((a, b) => {
+          // Извлечение числового значения из строки цены
+          const priceA = parseFloat(a.price.replace(/[^\d.]/g, ''));
+          const priceB = parseFloat(b.price.replace(/[^\d.]/g, ''));
+
+          // Сортировка по возрастанию или убыванию цены
+          return this.selectedSort === 'price_asc' ? priceA - priceB : priceB - priceA;
+        });
+      }
+    }
   },
   async mounted() {
     try {
       const response = await axios.get('http://localhost:3001/books/scrape');
       this.books = response.data;
+      this.sortBooks(); // Сортировать книги сразу после получения
     } catch (error) {
       console.error("There was an error fetching the books:", error);
     }
